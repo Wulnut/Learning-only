36098e1492aed149ee41dcedb8e5eb2529654d34
Wulnut <985951964@qq.com>
2020-01-19 21:47:13 +0800

    study

---
+++ pyhononly/ex37.py
@@ -0,0 +1,37 @@
+"""
+carlist = ["bbb","bbb","bbb","bbb","bbb","bbb"]
+
+print("carlsit:",carlist)
+"""
+
+"""
+carlist = ["aaa","bbb","ccc","ddd"]
+
+print("carlist[2]:",carlist[2])
+print("carlist[-1]:",carlist[-1])
+"""
+
+"""
+carlist = ["aaa","bbb","ccc","ddd","eee","fff","bbb"]
+
+print("carlist[2:5]:",carlist[2:5])
+print("carlist[2:5:2]:",carlist[2:5:2])
+"""
+
+"""
+carlist = ["aaa","bbb","ccc","ddd","eee","fff","bbb"]
+
+print("print:",end = ' ')
+
+for car in carlist:
+    print(car,end = ' ')
+"""
+
+carlist = ["aaa","bbb","ccc","ddd","eee","fff","bbb"]
+
+#carlist.append("ggg")
+#carlist.insert(2,"ggg")
+#carlist.extend(["yyy","ggg"])
+carlist[6:] = ["iii","ggg"] 
+
+print(carlist)
