b67cfc85172cf18b4989f304844442b7720eb767
Wulnut <985951964@qq.com>
2020-01-29 01:29:48 +0800

    stduy

--- C++only/ex10.cpp
+++ C++only/ex10.cpp
@@ -1,8 +1,60 @@
-#include<iostream>
+//dfs最短路径
+
+#include <bits/stdc++.h>
 using namespace std;
+
+char maze[105][105];
+bool vis[105][105];
+int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+int n, m;
+int ans = 100000;
+
+bool in(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
+void dfs(int x, int y, int steep) {
+    vis[x][y] = 1;
+
+    if (maze[x][y] == 'G'){
+        if (steep < ans){
+            ans = steep;
+        }
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        int tx = x + dir[i][0];
+        int ty = y + dir[i][1];
+        if ( in(tx, ty) && maze[tx][ty] != '#' && !vis[tx][ty]){
+            dfs(tx, ty, steep + 1);
+        }
+    }
+
+    vis[x][y] = 0;
+}
+
 int main(){
-    int n,m;
+   // freopen("note.txt", "r", stdin);
+    //freopen("ans.txt", "w", stdout);
     cin >> n >> m;
-    cout << n + m;
+
+    int x, y;
+    int steep = 0;
+
+    for ( int i = 0; i < n; ++i) {
+        cin >> maze[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j){
+            if (maze[i][j] == 'S'){
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    dfs(x, y, 0);
+    cout << ans << endl;
     return 0;
 }
