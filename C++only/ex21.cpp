312ed3af9783e6abd868e05268726a8c870c9252
Wulnut <985951964@qq.com>
2020-02-07 16:35:33 +0800

    stduy

---
+++ C++only/ex21.cpp
@@ -0,0 +1,36 @@
+//quick_sort
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 10000010;
+
+int n;
+int q[N];
+
+void quick_sort(int l, int r) {
+    if (l >= r) return ;
+
+    int i = 1, j = r + 1, x = q[(l + r) >> 1];
+    //q[(l + r) >> 1] 中的“>>”可以认为是向右移一位表示除2；愿意是位运算符 同理“<<”可以理解成乘2；
+
+    while(i < j) {
+        do i ++ ; while(q[i] < x);
+        do j -- ; while(q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+
+    quick_sort(l, j);
+    quick_sort(j + 1, r);
+}
+
+int main(){
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) cin >> q[i];
+
+    quick_sort(0, n - 1);
+
+    for (int i = 0; i < n; ++i) cout << q[i] <<endl;
+
+    return 0;
+}
---
+++ C++only/ex22.cpp
@@ -0,0 +1,33 @@
+//binary_search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100000;
+int q[N];
+int n = 0;
+
+bool binary_search(int x) {
+    int l = 0, r = n;
+
+    while(r - l >= 1) {
+        int i = (l +r) >> 1;
+        
+        if(q[i] == x) return true;
+        else if (q[i] < x) l = i + 1;
+        else r = i;
+    }
+
+    return false;
+}
+
+int main(){
+    cin >> n;
+
+    for (int i  = 0; i < n; ++ i) cin >> q[i];
+
+    sort(q, q + n);
+
+    binary_search(12);
+    return 0;
+}
