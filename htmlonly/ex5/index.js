1e04658ecc2388ae07d7f100f12b92341a134413
Wulnut <985951964@qq.com>
2020-06-20 21:26:49 +0800

    commit

---
+++ htmlonly/ex5/index.js
@@ -0,0 +1,14 @@
+
+    // var app = new Vue({
+    //     el: '#tag',
+    //     data: {
+    //         message: 'get'
+    //     }
+    // })
+
+    var app2 = new Vue({
+        el: '#app-2',
+        data: {
+          message: '页面加载于 ' + new Date().toLocaleString()
+        }
+      })
