7b4cf7e0c8758d3fd3826875c25d393de636eb4d
Wulnut <985951964@qq.com>
2020-06-23 16:20:36 +0800

    commit

--- MySTL/iterator.h
+++ MySTL/iterator.h
@@ -13,10 +13,10 @@ namespace MySTL {
     struct output_iterator_tag {};
     struct forward_iterator_tag : public input_iterator_tag {};
     struct bidirectional_iterator_tag : public forward_iterator_tag {};
-    struct random_iterator_tag : public bidirectional_iterator_tag {};
+    struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
     // iterator模板
-    template <class Category, class T, class Distance = ptrdiff_t,
+    template <class Category, class T, class Distance = ptrdiff_t, 
               class Pointer = T*, class Reference = T&>
               struct iterator {
 
@@ -50,7 +50,7 @@ namespace MySTL {
         typedef typename Iterator::value_type           value_type;
         typedef typename Iterator::pointer              pointer;
         typedef typename Iterator::reference            reference;
-        typedef typename Iterator::difference_type      defference_type;
+        typedef typename Iterator::difference_type      difference_type;
     };
 
     template <class Iterator, bool>
@@ -65,9 +65,217 @@ namespace MySTL {
 
     };
 
+    // 萃取迭代器的特性
+    template <class Iterator>
+    struct iterator_traits
+           : public iterator_triats_helper<Iterator, has_iterator_cat<Iterator>::value> {};
     
+    // 针对原生指针的偏化版本
+    template <class T>
+    struct iterator_traits<T*> {
+        
+        typedef random_access_iterator_tag  iterator_category;
+        typedef T                           value_type;
+        typedef T*                          pointer;
+        typedef T&                          reference;
+        typedef ptrdiff_t                   difference_type;
+    };
 
+    template <class T>
+    struct iterator_traits<const T*> {
 
+        typedef random_access_iterator_tag   iterator_category;
+        typedef T                            value_type;
+        typedef T*                           pointer;
+        typedef T&                           reference;
+        typedef ptrdiff_t                    difference_type;
+    };
+
+    template <class T, class U, bool = has_iterator_cat<iterator_traits<T>>::value>
+    struct has_iterator_cat_of
+           : public m_bool_constant<std::is_convertible
+             <typename iterator_traits<T>::iterator_category, U>::value>
+             {
+
+             };
+    
+   // 萃取某种迭代器
+   template <class T, class U> 
+   struct has_iterator_cat_of<T, U, false> : public m_false_type {};
+
+   template <class Iter>
+   struct is_input_iterator : public has_iterator_cat_of<Iter, input_iterator_tag> {};
+
+   template <class Iter>
+   struct is_ouput_iterator : public has_iterator_cat_of<Iter, output_iterator_tag> {};
+
+   template <class Iter>
+   struct is_forward_iterator : public has_iterator_cat_of<Iter, forward_iterator_tag> {};
+
+   template <class Iter>
+   struct is_bidirectional_iterator: public has_iterator_cat_of<Iter, bidirectional_iterator> {};
+
+   template <class Iter>
+   struct is_random_iterator : public has_iterator_cat_of<Iter, random_iterator> {};
+
+   template <class Iterator>
+   struct is_iterator
+          : public m_bool_constant<is_input_iterator<Iterator>::value || 
+            is_output_iterator<Iterator>::value> 
+        {
+
+        };
+
+    
+    // 萃取某个迭代器的Category
+    template <class Iterator>
+    typename iterator_traits<Iterator>::iterator_category
+    iterator_category(const Iterator) {
+
+        typedef typename iterator_traits<Iterator>::iterator_category Category;
+        return Category();
+    }
+
+    // 萃取某个迭代器的distance_type
+    template <class Iterator>
+    typename iterator_traits<Iterator>::difference_type*
+    distance_type(const Iterator&) {
+        return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+    }
+
+    // 萃取某个迭代器的value_type
+    template <class Iterator>
+    typename iterator_traits<Iterator>::value_type* 
+    value_type(const Iterator&) {
+        return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+    }
+
+    // 一下函数用于计算迭代器之间的距离
+
+    // distance 的 input_iterator_tag版本
+    template <class InputIterator>
+    typename iterator_traits<InputIterator>::difference_type
+    distance_dispatch(InputIterator first, InputIterator last, input_iterator_tag) {
+
+        typename iterator_traits<InputIterator>::difference_type n = 0;
+        while (first != last) {
+            ++ first;
+            ++ n;
+        }
+
+        return n;
+    }
+
+    // distance 的random_access_iterator_tag 的版本
+    template <class RandomIter>
+    typename iterator_traits<RandomIter>::difference_type
+    distance_dispathc(RandomIter first, RandomIter last, random_access_iterator_tag) {
+
+        return last - first;
+    }
+
+    template <class InputIterator>
+    typename iterator_traits<InputIterator>::difference_type 
+    distance(InputIterator first, InputIterator last) {
+
+        return distance_dispathc(first, last, iterator_category(first));
+    }
+
+    // 一下函数用于让迭代器前进n个距离
+
+    // advance 的 input_iterator_tag版本
+
+    template <class InputIterator, class Distance>
+    void advance_dispatch(InputIterator& i, Distance n, input_iterator_tag) {
+
+        while (n --) {
+             ++ i;
+        }
+    }
+
+    // advance de bidirectional_iterator_tag 的版本
+    template <class BidirectionalIterator, class Distance>
+    void advance_dispatch(BidirectionalIterator& i, Distance n, input_iterator_tag) {
+
+        if (n >= 0) {
+            while (n --) ++ i;
+        }
+        else {
+            while (n ++) -- i;
+        }
+    }
+
+    // advance 的random_access_iterator_tag的版本
+    template <class RandomIter, class Distance>
+    void advance_dispatch(RandomIter& i, Distance n, random_access_iterator_tag) {
+
+        i += n;
+    }
+
+    template <class InputIterator, class Distance>
+    void advance(InputIterator& i, Distance n) {
+
+        advance_dispatch(i, n, iterator_category(i));
+    }
+
+    /********************************************************************************************************/
+
+    // 模板类：reverse_iterator
+    // 代表反向迭代器， 使前进为后退， 后退为前进
+
+    template <class Iterator>
+    class reverse_iterator {
+        private:
+            Iterator current; // 记录对应的反向迭代器
+        
+        public: 
+            // 反向迭代器的五种相应型别
+            typedef typename iterator_traits<Iterator>::iterator_category  iterator_category;
+            typedef typename iterator_traits<Iterator>::value_type         value_type;
+            typedef typename iterator_traits<Iterator>::difference_type    difference_type;
+            typedef typename iterator_traits<Iterator>::pointer            pointer;
+            typedef typename iterator_traits<Iterator>::reference          reference;
+
+            typedef Iterator                                                iterator_type;
+            typedef reverse_iterator<Iterator>                              self;
+        
+        public:
+            // 构造函数
+            reverse_iterator() = default;
+            explicit reverse_iterator(iterator_type i) : current(i) { }
+            reverse_iterator(const self& rhs) : current(rhs.current) { }
+        
+        public:
+            // 取出对应的正向迭代器
+            iterator_type base() const { return current; }
+
+            // 重载操作符
+            reference operator*() const {
+
+                // 实际对应正向迭代器的前一个位置
+                auto tmp = current;
+                return *--tmp;
+            }
+
+            pointer operator->() const {
+                
+                return &(operator* ());
+            }
+
+            // 前进(++)b变为后退(--)
+            self& operatot++() {
+
+                -- current;
+                return *this;
+            }
+
+            self operator++(int) {
+
+                self tmp = *this;
+                -- current;
+                return tmp;
+            }
+    }
 
 } // namespace MySTL
 
